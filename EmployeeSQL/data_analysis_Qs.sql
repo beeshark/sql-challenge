fede76b11b8704047d26dc02df1367428d7c60c5
Sara <sbehnke33@yahoo.com>
2020-10-06 22:09:46 -0500

    Update data_analysis_Qs.sql

--- EmployeeSQL/data_analysis_Qs.sql
+++ EmployeeSQL/data_analysis_Qs.sql
@@ -1,8 +1,14 @@
 -- #List the following details of each employee: employee number, last name, first name, sex, and salary.
-
+select Employees.emp_no, Employees.last_name, Employees.first_name, Employees.gender, Salaries.salary
+from Employees
+inner join Salaries on
+Employees.emp_no = Salaries.emp_no;
 
 -- #List first name, last name, and hire date for employees who were hired in 1986.
-
+select * from Employees
+where hire_date >= '1985-12-13'
+and hire_date < '1987-01-01'
+order by emp_no;
 
 -- #List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
 
